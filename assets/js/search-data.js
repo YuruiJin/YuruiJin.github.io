6d7e365dd2589460db942a57ad9f8dfa5e23c7ec
YuruiJin <96754673+YuruiJin@users.noreply.github.com>
2026-02-19 19:39:59 +0000

    Deploying to gh-pages from @ YuruiJin/YuruiJin.github.io@bf034b17a49d314254a53a2ed979de87a305f2a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,8 +14,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-关于我",
-    title: "关于我",
+    id: "nav-about",
+    title: "About",
     section: "Navigation menu",
     handler: () => {
       window.location.href = "/";
